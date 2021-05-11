d9c06c143cf14c73ae6f7076e2fd26820d611b21
Paulo <paulo.leite@academico.ifpb.edu.br>
2021-05-11 18:54:15 -0300

    Buscar categorias de filmes na api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Tmdb from './Tmdb';
 
 export default () => {
+
+  useEffect(() => {
+    const loadAll = async () => {
+      // Pegando a lista total
+      let list = await Tmdb.getHomeList(); 
+      console.log(list);
+    }
+
+    loadAll();
+  }, []);
+
   return (
     <div>
       Olá mundo
