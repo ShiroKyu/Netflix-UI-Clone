ec7592e0a036a1a7e60813aa7c4c464c758e3744
Paulo <paulo.leite@academico.ifpb.edu.br>
2021-05-11 19:24:29 -0300

    Base interface plus hover effect on poster

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Tmdb from './Tmdb';
+import './App.css'
+
+import MovieRow from './components/MovieRow';
 
 export default () => {
+
+  const [movieList, setMovieList] = useState([]);
 
   useEffect(() => {
     const loadAll = async () => {
       // Pegando a lista total
       let list = await Tmdb.getHomeList(); 
-      console.log(list);
+      setMovieList(list);
     }
 
     loadAll();
   }, []);
 
   return (
-    <div>
-      Olá mundo
+    <div className="page">
+      <section className="lists">
+        { movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items}/>
+        )) }
+      </section>
     </div>
   )
 }
